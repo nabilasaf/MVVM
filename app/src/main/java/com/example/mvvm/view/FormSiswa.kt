8273d06fe04a75eada4849374be1fa39eb8bb6ec
nabilasaf <nabilasafitri861@gmail.com>
2025-11-20 12:10:21 +0700

    menambahkan FormSiswa

---
+++ app/src/main/java/com/example/mvvm/view/FormSiswa.kt
@@ -0,0 +1,55 @@
+package com.example.mvvm.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mvvm.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+
+@Composable
+fun FormSiswa (modifier: Modifier,
+               pilihanJK: List<String>,
+              onSubmit:(MutableList<String>)-> Unit)
+{
+    var textNama by remember { mutableStateOf(value = " ") }
+    var textAlamat by remember { mutableStateOf(value = " ") }
+    var textGender by remember { mutableStateOf(value = " ") }
+    val listData: MutableList<String> = mutableListOf(textNama, textAlamat, textGender)
+}
